6d3137c3f8df5d33bed9a3fed5657872926616bc
mnene-william <mnenewilliam26@gmail.com>
2025-08-12 12:04:11 +0300

    Create a photo model and migrate it

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = CloudinaryField('image', null=True)
+    tags = models.CharField(max_length=200, help_text='Comma-separated tags')
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
+
+    def __str__(self):
+        return self.title
